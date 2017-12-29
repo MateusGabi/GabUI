fd37d1617bc57945a70b221212902d6ff8e311ef
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-12-29 11:43:30 -0300

    Adicionado Pug

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp')
+const serve = require('gulp-serve')
+const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const minifyCSS = require('gulp-csso')
 
-gulp.task('style', () => {
+gulp.task('css', () => {
     return gulp
         .src('./scss/index.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/assets/css'))
 })
 
-gulp.task('default', ['style'])
+gulp.task('html', function () {
+    return gulp.src('./examples/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./dist/examples/'))
+})
+
+gulp.task('serve', serve('dist'))
+
+gulp.task('default', ['html', 'css', 'serve'])
