e89a10b5761bdfb3d758281de3c27b9778d6d866
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-01-02 18:02:49 -0300

    add gulp plugins

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,55 @@
+const fs = require('fs')
+
 const gulp = require('gulp')
 const serve = require('gulp-serve')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const minifyCSS = require('gulp-csso')
+const licenser = require('gulp-header-license')
+const concat = require("gulp-concat")
+const cleanDest = require('gulp-clean-dest')
+
+const runSequence = require('run-sequence')
+
+const DEST_CSS = './dist/assets/css'
+const DEST_HTML = './dist/docs/'
+const DEST_JS = './dist/assets/js'
+
+var getCopyright = () => {
+    var year = (new Date()).getFullYear();
+    var license = getFile('LICENSE.md');
+
+    return `\\* \n\ ${license}  \n\ */`;
+};
+
+var getFile = name => fs.readFileSync(name)
 
 gulp.task('css', () => {
+
     return gulp
         .src('./scss/index.scss')
+        .pipe(cleanDest(DEST_CSS))
         .pipe(sass())
-        .pipe(gulp.dest('./dist/assets/css'))
+        .pipe(minifyCSS())
+        // .pipe(licenser(getCopyright(), {}))
+        .pipe(gulp.dest(DEST_CSS))
 })
 
 gulp.task('html', () => {
     return gulp
         .src('./docs/**/*.pug')
+        .pipe(cleanDest(DEST_HTML))
         .pipe(pug())
-        .pipe(gulp.dest('./dist/docs/'))
+        .pipe(gulp.dest(DEST_HTML))
 })
 
 gulp.task('js', () => {
     return gulp
         .src('./javascript/index.js')
-        .pipe(gulp.dest('./dist/assets/js'))
+        .pipe(cleanDest(DEST_JS))
+        .pipe(gulp.dest(DEST_JS))
 })
 
 gulp.task('serve', serve('dist'))
-gulp.task('dist', ['html', 'css', 'js'])
+gulp.task('dist', ['css', 'html', 'js'])
 gulp.task('default', ['dist', 'serve'])
